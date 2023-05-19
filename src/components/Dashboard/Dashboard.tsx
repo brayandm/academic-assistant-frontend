ddd5d9939ec89a0470efc5fdfd5d60b3a0642b6c
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-05-19 23:40:33 +0200

    updating dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Toolbar,
@@ -8,14 +8,56 @@ import {
   Container,
   Grid,
   Paper,
+  Button,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 
-const Dashboard = () => {
+const Dashboard: React.FC = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleUserPage = () => {
+    // Logic to navigate to the user page
+  };
+
+  const handleLogout = () => {
+    // Logic to handle logout
+  };
+
   return (
     <div>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6">Dashboard</Typography>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Dashboard
+          </Typography>
+          <Button color="inherit" onClick={handleClick}>
+            Menu
+          </Button>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+          >
+            <MenuItem onClick={handleUserPage}>Go to User Page</MenuItem>
+            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
       <Container sx={{ paddingTop: "24px" }}>
