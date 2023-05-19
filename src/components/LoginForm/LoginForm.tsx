56c657aecc51ed00b3a0999a86fa1d085d941444
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-05-19 23:18:01 +0200

    Material UI used

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import * as React from "react";
+import Button from "@mui/material/Button";
+
 export default function LoginForm() {
-  return <p>Login Form</p>;
+  return (
+    <div>
+      <Button variant="contained">Login</Button>
+    </div>
+  );
 }
