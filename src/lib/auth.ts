6975da65499989419388da955d1a60ca93871fb4
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-05-20 06:15:57 +0200

    some libraries deleted

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,5 @@
 import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { getServerSession } from "next-auth/next";
-import { redirect } from "next/navigation";
 
 export const authOptions: NextAuthOptions = {
   providers: [
