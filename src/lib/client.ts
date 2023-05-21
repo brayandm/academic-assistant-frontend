e218f72c5b09b131365fb41a1cb5fcfa5a9f3b98
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-05-21 09:28:06 +0200

    client and server code generation added

---
+++ src/lib/client.ts
@@ -0,0 +1,14 @@
+import { GraphQLClient } from "graphql-request";
+import { getSdk } from "@/graphql/server";
+
+export const GRAPHQL_ENDPOINT = "http://localhost:83/graphql";
+
+const client = new GraphQLClient(GRAPHQL_ENDPOINT, {
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const sdk = getSdk(client);
+
+export default client;
