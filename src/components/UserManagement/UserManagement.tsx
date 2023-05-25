8a08e685c9420e24de7c9a87c546f730a9607b90
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-05-25 20:12:22 +0200

    Tab section added

--- src/components/UserManagement/UserManagement.tsx
+++ src/components/UserManagement/UserManagement.tsx
@@ -1,111 +1,23 @@
 import * as React from "react";
-import { useTheme } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  dir?: string;
-  index: number;
-  value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `full-width-tab-${index}`,
-    "aria-controls": `full-width-tabpanel-${index}`,
-  };
-}
+import TabSection from "@/components/TabSection";
 
 export default function UserManagement() {
-  const theme = useTheme();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
-
-  const handleChangeIndex = (index: number) => {
-    setValue(index);
-  };
-
   return (
-    <Box sx={{ width: "86vw" }}>
-      <Box>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="secondary"
-          textColor="inherit"
-          variant="fullWidth"
-          aria-label="full width tabs example"
-          sx={{
-            backgroundColor: theme.palette.secondary.dark,
-            "& 	.MuiTabs-indicator": {
-              backgroundColor: theme.palette.secondary.light,
-              top: "5vh",
-              height: "0.5vh",
-            },
-            "& .MuiTab-root": {
-              fontSize: "16px",
-            },
-          }}
-        >
-          <Tab
-            label="Item One"
-            sx={{
-              color: theme.palette.common.white,
-            }}
-            {...a11yProps(0)}
-          />
-          <Tab
-            label="Item Two"
-            sx={{
-              color: theme.palette.common.white,
-            }}
-            {...a11yProps(1)}
-          />
-          <Tab
-            label="Item Three"
-            sx={{
-              color: theme.palette.common.white,
-            }}
-            {...a11yProps(2)}
-          />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0} dir={theme.direction}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1} dir={theme.direction}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2} dir={theme.direction}>
-        Item Three
-      </TabPanel>
-    </Box>
+    <TabSection
+      tabs={[
+        {
+          label: "Users",
+          component: <div>Users</div>,
+        },
+        {
+          label: "Roles",
+          component: <div>Roles</div>,
+        },
+        {
+          label: "Permissions",
+          component: <div>Permissions</div>,
+        },
+      ]}
+    />
   );
 }
