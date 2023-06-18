e8922ffb601ccf65e8e312226c00cc8d6a8d8770
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-06-18 22:29:39 +0200

    Create User form added

--- src/components/UsersTable/UsersTable.tsx
+++ src/components/UsersTable/UsersTable.tsx
@@ -8,6 +8,33 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { useUsersQuery } from "@/graphql/hooks";
 import { useSession } from "next-auth/react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  IconButton,
+  Modal,
+  TextField,
+  Typography,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import AddIcon from "@mui/icons-material/Add";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  display: "flex",
+  flexDirection: "column",
+  p: 4,
+};
 
 export default function UsersTable() {
   const { data: session } = useSession();
@@ -20,8 +47,20 @@ export default function UsersTable() {
     },
   });
 
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
+      <Button
+        variant="contained"
+        endIcon={<AddIcon />}
+        sx={{ marginBottom: "20px" }}
+        onClick={handleOpen}
+      >
+        Create User
+      </Button>
       {data && (
         <TableContainer sx={{ width: 700 }} component={Paper}>
           <Table sx={{ minWidth: 700 }} aria-label="simple table">
@@ -31,6 +70,8 @@ export default function UsersTable() {
                 <TableCell align="right">Name</TableCell>
                 <TableCell align="right">Email</TableCell>
                 <TableCell align="right">Roles</TableCell>
+                <TableCell align="center">Update</TableCell>
+                <TableCell align="center">Delete</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -50,12 +91,58 @@ export default function UsersTable() {
                       return role.name;
                     })}
                   </TableCell>
+                  <TableCell align="center">
+                    <IconButton aria-label="delete" size="large">
+                      <EditIcon fontSize="inherit" />
+                    </IconButton>
+                  </TableCell>
+                  <TableCell align="center">
+                    <IconButton aria-label="delete" size="large">
+                      <DeleteIcon fontSize="inherit" />
+                    </IconButton>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
       )}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <TextField
+            id="outlined-basic"
+            label="Name"
+            variant="outlined"
+            sx={{ marginBottom: "20px" }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            sx={{ marginBottom: "20px" }}
+          />
+          <Typography variant="h6" gutterBottom component="div">
+            Roles:
+          </Typography>
+          <FormGroup sx={{ flexDirection: "row" }}>
+            <FormControlLabel control={<Checkbox />} label="Student" />
+            <FormControlLabel control={<Checkbox />} label="Teacher" />
+            <FormControlLabel control={<Checkbox />} label="Admin" />
+          </FormGroup>
+          <Button
+            variant="contained"
+            endIcon={<AddIcon />}
+            sx={{ alignSelf: "center", marginTop: "40px" }}
+          >
+            Create
+          </Button>
+        </Box>
+      </Modal>
     </>
   );
 }
