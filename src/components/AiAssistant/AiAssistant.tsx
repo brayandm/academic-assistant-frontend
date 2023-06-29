a87ca02d50842affd984c6bf225a0804a8fe46ce
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-06-29 00:33:56 +0000

    adding AItalker component

--- src/components/AiAssistant/AiAssistant.tsx
+++ src/components/AiAssistant/AiAssistant.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { Typography, Container } from "@mui/material";
+import AiTalker from "@/components/AiTalker";
 
 function AiAssistant() {
   return (
@@ -9,10 +10,25 @@ function AiAssistant() {
       <Container sx={{ paddingTop: "24px" }}>
         <Typography
           variant="h2"
-          sx={{ marginTop: "200px", textAlign: "center" }}
+          sx={{ marginTop: "100px", textAlign: "center" }}
         >
           AI Assistant
         </Typography>
+      </Container>
+      <Container
+        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+      >
+        <AiTalker
+          openAiGptStreamerUrl={
+            process.env.NEXT_PUBLIC_OPENAI_GPT_STREAMER_URL || ""
+          }
+          awsPollyStreamerUrl={
+            process.env.NEXT_PUBLIC_AWS_POLLY_STREAMER_URL || ""
+          }
+          awsTranscribeStreamerUrl={
+            process.env.NEXT_PUBLIC_AWS_TRANSCRIBE_STREAMER_URL || ""
+          }
+        />
       </Container>
     </div>
   );
